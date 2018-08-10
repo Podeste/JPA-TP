7a589ac309caf0d5802743e910db42e862286313
Unknown <paul.tauzia@soprasteria.com>
2018-08-10 12:17:24 +0200

    Commiiiiiiuiiiiiiiiit

---
+++ formation-jpa/src/test/java/Test.java
@@ -0,0 +1,44 @@
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Test {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("eshop");
+		
+		EntityManager em = null;
+		
+		EntityTransaction tx = null;
+		
+//		try {
+//			em = emf.createEntityManager();
+//			tx = em.getTransaction();
+//			List<Produit> myProduits = em.createQuery("select p from Produit p", Produit.class).getResultList();
+//		//...
+//		//On oublie pas de fermer EntityManager et EntityManagerFactory
+//			for (Produit produit : myProduits) {
+//				System.out.println(produit.getLibelle());
+//			}
+//			tx.commit();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			if (tx!=null){
+//				tx.rollback();
+//			}
+//		} finally {
+//			if (em!=null) {
+//				em.close();
+//			}
+//		}
+		em.close();
+		
+		emf.close();
+
+		}
+	}
+
+}
